9b9c3dd6af2f60098d5f72251633a5b225aecb0b
pktdanh <pktdanh@gmail.com>
2024-06-13 00:02:06 +0700

    Update callbacks next-auth

---
+++ next-auth.d.ts
@@ -0,0 +1,24 @@
+import { UserRole } from '@prisma/client';
+import NextAuth, { type DefaultSession } from 'next-auth';
+import { JWT } from 'next-auth/jwt';
+
+export type ExtendedUser = DefaultSession['user'] & {
+  role: UserRole;
+};
+
+declare module 'next-auth' {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: ExtendedUser;
+  }
+}
+
+declare module 'next-auth/jwt' {
+  /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
+  interface JWT {
+    /** OpenID ID Token */
+    role?: UserRole;
+  }
+}
