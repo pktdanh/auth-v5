a7cd49a3f5e7c239847d5c1b64317b16815f622e
pktdanh <pktdanh@gmail.com>
2024-06-18 23:35:56 +0700

    User button

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,18 +1,17 @@
-import { auth, signOut } from '@/auth';
+'use client';
 
-export default async function SettingsPage() {
-  const session = await auth();
+import { useCurrentUser } from '@/hooks/use-current-user';
+import { signOut } from 'next-auth/react';
+
+export default function SettingsPage() {
+  const session = useCurrentUser();
+
+  const onClick = () => {
+    signOut();
+  };
   return (
-    <div>
-      {JSON.stringify(session)}
-      <form
-        action={async () => {
-          'use server';
-          await signOut();
-        }}
-      >
-        <button type="submit">Log out</button>
-      </form>
+    <div className="bg-white p-10 rounded-xl">
+      <button onClick={onClick}>Log out</button>
     </div>
   );
 }
